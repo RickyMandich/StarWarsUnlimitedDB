b91278ed2197e601f7f01d0b1cfe92fa05ac18df
RickyMandich <ricky.mandich@gmail.com>
2024-09-09 11:49:39 +0200

    aggiornamento 2024 09 09 11:49

--- src/main/java/com/example/starwarsunlimiteddb/StarWarsUnlimitedDbApplication.java
+++ src/main/java/com/example/starwarsunlimiteddb/StarWarsUnlimitedDbApplication.java
@@ -12,6 +12,25 @@ import java.sql.*;
 @SpringBootApplication
 @Controller
 public class StarWarsUnlimitedDbApplication {
+
+    @GetMapping("/insertToDB")
+	public ModelAndView insertToDB(){
+		return new ModelAndView("insertToDB");
+	}
+
+    @GetMapping("/insertToDB/operation")
+    public ModelAndView insertToDBOperation(){
+        System.out.println("form ricevuto");
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/starwarsunlimited", "root", "Minecraft35?")) {
+            try (Statement stmt = conn.createStatement()) {
+                System.out.println("ho applicato " + stmt.executeUpdate("insert into carte values ()") + " modifiche");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return new ModelAndView("insertToDB");
+    }
+
     @GetMapping("/insertToDeck")
 	public ModelAndView insertToDeck(){
 		return new ModelAndView("insertToDeck");
@@ -20,10 +39,10 @@ public class StarWarsUnlimitedDbApplication {
     @GetMapping("/insertToDeck/operation")
     public ModelAndView insertToDeckOperation(@RequestParam String mazzo,@RequestParam String espansione,@RequestParam String nr){
         System.out.println("form ricevuto");
-        try (Connection conn = DriverManager.getConnection("jdbc:mysql://10.0.8.170:3306/world", "formazione", "129EL@doiej!")) {
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/starwarsunlimited", "root", "Minecraft35?")) {
             try (Statement stmt = conn.createStatement()) {
                 System.out.println("sono dentro il try");
-                System.out.println("ho applicato " + stmt.executeUpdate("insert into mazzi values ('" + mazzo + "', '" + espansione + "', '" + nr + "')") + " modifiche");
+                System.out.println("ho applicato " + stmt.executeUpdate("insert into mazzi values ('" + mazzo.toLowerCase() + "', '" + espansione.toUpperCase() + "', '" + nr + "')") + " modifiche");
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
